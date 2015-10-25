7d773f98a2692bfee4203237b890cf54acdf7f73
Chuan Jin <chuan.jin.813@gmail.com>
2015-10-25 18:10:47 +0100

    Done

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,87 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  sum = 0
+  arr.each {|x| sum += x}
+  sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr.empty?
+    0
+  elsif arr.length == 1
+    arr[0]
+  else
+    r_arr = arr.sort.reverse
+    r_arr[0] + r_arr[1]
+  end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  if arr.empty? or arr.length == 1
+    false
+  else
+
+    i = 0
+    while i < arr.length - 1 do
+      j = i+1
+      while j < arr.length do
+        return true if arr[i] + arr[j] == n
+        j=j+1
+      end
+      i=i+1
+    end
+    return false
+  end
+
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, " + "#{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  return false if s.empty?
+
+  vowls = ['a','e','i','o','u']
+  alphabet = ('a'..'z').to_a
+
+  conlist = alphabet - vowls
+
+  uconlist = conlist.map(&:upcase)
+  final_arry = conlist + uconlist
+
+  for elem in final_arry
+    return true if s.start_with?(elem)
+  end
+  false
+
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return true if s == '0'
+  d = s.to_i(2)
+  return false if d == 0
+  return true if d % 4 == 0
+  return false
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+
+  def initialize(isbn, price)
+    raise ArgumentError if isbn.empty? or price <= 0
+    @isbn = isbn
+    @price = price
+
+  end
+
+  def price_as_string
+    '$' + '%.2f' % @price
+
+  end
 end
